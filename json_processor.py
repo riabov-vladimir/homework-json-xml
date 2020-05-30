8091d0a557a041d460116173cd1c1a5c807b0631
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-05-30 16:58:03 +0300

    .json words_query dict where key is a unique word and value is it's frequency

--- json_processor.py
+++ json_processor.py
@@ -9,6 +9,20 @@ news_text = []
 
 for item in news_feed['rss']['channel']['items']:
 	for word in item['description'].split():
-		news_text.append(word)
+		news_text.append(word.lower())
 
-print(news_text)
+# news_text = filter(lambda x: len(x)>6, news_text)
+
+# news_text.sort(key=lambda x: len(x))
+
+news_text_sorted = list(filter(lambda x: (len(x)>6), news_text))
+
+words_query = {}
+
+for word in news_text_sorted:
+	if word in words_query.keys():
+		words_query[word] += 1
+	else:
+		words_query.setdefault(word, 1)
+
+pprint(words_query)
