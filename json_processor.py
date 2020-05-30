405f9d06a093151e4dba88cd4f20867aa2fac73e
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-05-30 20:05:13 +0300

    На проверку!

--- json_processor.py
+++ json_processor.py
@@ -2,6 +2,9 @@ import json
 
 
 def json_news_text_reader(file='newsafr.json'):
+	"""Функция для десериализации xml файла с новостями в список всех слов всех новостей.
+	Принимает название файла (файл по умолчанию 'newsafr.xml'), возвращает список news_text."""
+
 	with open(file, 'r', encoding='UTF-8') as reader:
 		news_feed = json.load(reader)
 
@@ -10,4 +13,5 @@ def json_news_text_reader(file='newsafr.json'):
 	for item in news_feed['rss']['channel']['items']:
 		for word in item['description'].split():
 			news_text.append(word.lower())
+
 	return news_text
--- xml_processor.py
+++ xml_processor.py
@@ -2,6 +2,9 @@ import xml.etree.ElementTree as ET
 
 
 def xml_news_text_reader(file='newsafr.xml'):
+	"""Функция для десериализации xml файла с новостями в список всех слов всех новостей.
+	Принимает название файла (файл по умолчанию 'newsafr.xml'), возвращает список news_text."""
+
 	parser = ET.XMLParser(encoding='utf-8')
 	tree = ET.parse(file, parser)
 	root = tree.getroot()
@@ -10,10 +13,10 @@ def xml_news_text_reader(file='newsafr.xml'):
 
 	news_texts = []
 
-	for item in xml_items:
+	for item in xml_items: # не догадался как сделать, чтобы сразу получился список, а не список списков
 		news_texts.append((item.find('description').text.lower().split()))
 
-	news_text = []
+	news_text = [] # список списков --> список
 
 	for text in news_texts:
 		news_text.extend(text)
