a0f4fb8d05334f5dca4db44a1aac8de6dc5528d5
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-05-30 15:01:15 +0300

    .json news_text is a list of all words from all of the news

--- json_processor.py
+++ json_processor.py
@@ -4,4 +4,11 @@ from pprint import pprint
 
 with open('newsafr.json', 'r', encoding='UTF-8') as reader:
 	news_feed = json.load(reader)
-	pprint(news_feed.get(''))
+
+news_text = []
+
+for item in news_feed['rss']['channel']['items']:
+	for word in item['description'].split():
+		news_text.append(word)
+
+print(news_text)
